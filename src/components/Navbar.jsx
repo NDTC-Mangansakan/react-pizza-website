9e97b92a05629d883711a84d1c009371d784313a
Shaq <mangansakansansudin@gmail.com>
2025-08-06 07:56:53 +0800

    iniital commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='sticky top-0 right-0 left-0 h-[10vh] flex items-center'>
+        <div className="custom-container p-5 border">
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
